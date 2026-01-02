796399e108f6602d771106a6be132938bfaddea8
barb4rian <aat.sambhavrocksingh123@gmail.com>
2026-01-02 19:55:04 +0530

    day39_1207

---
+++ day39_1207.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        int[] count = new int[2001];
+        for (int x : arr) 
+            count[x + 1000]++;
+        
+        boolean[] seen = new boolean[1001];
+        for (int c : count) {
+            if (c > 0) {
+                if (seen[c]) return false;
+                seen[c] = true;
+            }
+        }
+        return true;
+    }
+}
