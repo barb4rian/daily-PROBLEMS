5e0ffca89e020984bce0027d54568f9b940286fe
barb4rian <aat.sambhavrocksingh123@gmail.com>
2026-01-10 22:20:38 +0530

    day54_2095

---
+++ day54_2095.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head == null || head.next == null) return null;
+        
+        ListNode dummy = new ListNode(0, head);
+        ListNode slow = dummy;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        //  (which is the middle)
+        slow.next = slow.next.next;
+        
+        return dummy.next;
+    }
+}
