1aeeea6c7f02d8a0fbd6a0bedb9cb9d575167467
barb4rian <aat.sambhavrocksingh123@gmail.com>
2026-01-19 20:50:56 +0530

    day73_236

---
+++ day73_236.java
@@ -0,0 +1,16 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode r, TreeNode p, TreeNode q) {
+        if (r == null || r == p || r == q) {
+            return r;
+        }
+        
+        TreeNode l = lowestCommonAncestor(r.left, p, q);
+        TreeNode ri = lowestCommonAncestor(r.right, p, q);
+        
+        if (l != null && ri != null) {
+            return r;
+        }
+        
+        return l != null ? l : ri;
+    }
+}
