849f12a6fd36b761c046a241d87815ba513ef087
barb4rian <aat.sambhavrocksingh123@gmail.com>
2026-04-09 18:46:01 +0530

    day224_1

---
+++ day224_1.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        
+        return new int[]{}; // Should never reach here (problem guarantees a solution)
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        
+        int[] result = sol.twoSum(nums, target);
+        System.out.println(result[0] + ", " + result[1]); // Output: 0, 1
+    }
+}
